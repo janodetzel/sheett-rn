20b78e2daee85b9ae215ef8af53de65fab3e0a7a
Jano Detzel <jano.detzel@gmail.com>
2025-05-30 17:13:29 +0200

    Metro config

---
+++ packages/Sheett/metro.config.js
@@ -0,0 +1,17 @@
+const { getDefaultConfig } = require("expo/metro-config");
+const path = require("path");
+
+// Find the project and workspace directories
+const projectRoot = __dirname;
+// This can be replaced with `find-yarn-workspace-root`
+const monorepoRoot = path.resolve(projectRoot, "../..");
+
+const config = getDefaultConfig(projectRoot);
+
+config.watchFolders = [monorepoRoot];
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, "node_modules"),
+  path.resolve(monorepoRoot, "node_modules"),
+];
+
+module.exports = config;
