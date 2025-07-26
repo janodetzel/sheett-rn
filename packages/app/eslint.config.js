fa088db6143ff1a87bc744bf8b20dae423d02cb3
Jano Detzel <jano.detzel@gmail.com>
2025-07-26 14:48:24 +0200

    Remove Drizzle

--- packages/app/eslint.config.js
+++ packages/app/eslint.config.js
@@ -1,17 +1,10 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require("eslint/config");
 const expoConfig = require("eslint-config-expo/flat");
-const drizzle = require("eslint-plugin-drizzle");
 
 module.exports = defineConfig([
   expoConfig,
   {
     ignores: ["dist/*"],
-  },
-  {
-    plugins: { drizzle },
-    rules: {
-      ...drizzle.configs.recommended.rules,
-    },
   },
 ]);
